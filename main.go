a31a5d2c8d4bb9ee5ac45e3f38edd435f70b16c5
Masud Rana <masud.aiub.cse@gmail.com>
2025-04-28 21:52:39 +0200

    tc system added

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 )
 
+var totalTickets = 100
+
 func main() {
-	fmt.Printf("Hello and welcome")
+
+	for totalTickets > 0 {
+		var ticket = 0
+		fmt.Println("Welcome to the ticketBooking system!")
+		fmt.Println("Available tickets: ", totalTickets)
+
+		fmt.Println("Please enter ano of tickets: ")
+		fmt.Scan(&ticket)
+
+		if totalTickets > ticket && totalTickets > 0 && ticket > 0 {
+			totalTickets = totalTickets - ticket
+		} else {
+			fmt.Println("Please enter valid number of ticket !!!!")
+			continue
+		}
+
+		uuid := uuid.New()
+		fmt.Println("Here is the code: ", uuid)
+
+		fmt.Println("Currently Available tickets: ", totalTickets)
+	}
 }
